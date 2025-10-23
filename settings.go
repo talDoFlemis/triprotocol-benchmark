da99a18be181f5e2e746f6e44d9d8d037460973f
Said Rodrigues <said.cavalcante.rodrigues@sap.com>
2025-10-23 13:16:12 -0300

    feat: add settings

---
+++ settings.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+
+	_ "embed"
+
+	"github.com/spf13/viper"
+)
+
+//go:embed base.yaml
+var BaseSettings []byte
+
+type CORSSettings struct {
+	Origins []string `mapstructure:"origins" validate:"min=1,dive,url"`
+	Methods []string `mapstructure:"methods" validate:"min=1,dive,oneof=GET POST PUT DELETE OPTIONS PATCH HEAD"`
+	Headers []string `mapstructure:"headers" validate:"min=1"`
+}
+
+type HTTPSettings struct {
+	Port    string       `mapstructure:"port" validate:"required,numeric"`
+	Prefix  string       `mapstructure:"prefix" validate:"required"`
+	IP      string       `mapstructure:"ip" validate:"required,ip"`
+	CORS    CORSSettings `mapstructure:"cors" validate:"required"`
+	Timeout int          `mapstructure:"timeout" validate:"gte=1"`
+}
+
+type ObservabilitySettings struct {
+	Enabled  bool   `mapstructure:"enabled"`
+	Endpoint string `mapstructure:"endpoint" validate:"required_if=Enabled true,url"`
+}
+
+type OpenTelemetryLogSettings struct {
+	TimeoutInSec  int64 `mapstructure:"timeout"`
+	IntervalInSec int64 `mapstructure:"interval"`
+	MaxQueueSize  int   `mapstructure:"maxqueuesize"`
+	BatchSize     int   `mapstructure:"batchsize"`
+}
+
+type OpenTelemetryTraceSettings struct {
+	TimeoutInSec int64 `mapstructure:"timeout"`
+	MaxQueueSize int   `mapstructure:"maxqueuesize"`
+	BatchSize    int   `mapstructure:"batchsize"`
+	SampleRate   int   `mapstructure:"samplerate"`
+}
+
+type OpenTelemetryMetricSettings struct {
+	IntervalInSec int64 `mapstructure:"interval"`
+	TimeoutInSec  int64 `mapstructure:"timeout"`
+}
+
+type OpenTelemetrySettings struct {
+	Enabled  bool                        `mapstructure:"enabled"`
+	Endpoint string                      `mapstructure:"endpoint"`
+	Metrics  OpenTelemetryMetricSettings `mapstructure:"metrics"`
+	Traces   OpenTelemetryTraceSettings  `mapstructure:"traces"`
+	Logs     OpenTelemetryLogSettings    `mapstructure:"logs"`
+	Interval int                         `mapstructure:"interval"`
+}
+
+type AppSettings struct {
+	Name    string `mapstructure:"name"`
+	Version string `mapstructure:"version"`
+	Env     string `mapstructure:"env"`
+}
+
+type Settings struct {
+	App  AppSettings  `mapstructure:"app" validate:"required"`
+	HTTP HTTPSettings `mapstructure:"http" validate:"required"`
+}
+
+func LoadConfig[T any](prefix string, baseConfig []byte) (*T, error) {
+	var cfg *T
+
+	viper.SetConfigType("yaml")
+	err := viper.ReadConfig(bytes.NewReader(baseConfig))
+	if err != nil {
+		log.Println("Failed to read config from yaml")
+		return nil, err
+	}
+
+	viper.SetEnvPrefix(prefix)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", ""))
+	viper.AutomaticEnv()
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
